40f8f57c18c9aca70e4c072592e5874e00a2849f
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-11 15:25:19 -0300

    Criação da Classe Products

---
+++ Orders/Entities/Products.cs
@@ -0,0 +1,15 @@
+﻿namespace Orders.Entities {
+    class Products {
+        public string Name { get; set; }
+
+        public double price { get; set; }
+
+        public Products() { 
+        
+        }
+        public Products(string name, double price) {
+            Name = name;
+            this.price = price;
+        }
+    }
+}
